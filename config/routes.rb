0dd465f3e2740b27d92abddbf698bfb2722c389f
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 14:36:57 -0500

    cleanup code for user model, view, controller, routes
    remove all comments from user model
    removed unneccesary comments from routes.rb
    removed except/only and as/homepage from config.rb
    added development and test sqlite3 files plust the log directory to .gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 StackOverflowClone::Application.routes.draw do
-  #USER ROUTES
-  root to: 'pages#index', as: 'homepage'
-  resources :users, except: [:index]
+  root to: 'pages#index'
+  resources :users
   get '/login', to: 'sessions#new'
 
-
-  #SESSION ROUTES
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
