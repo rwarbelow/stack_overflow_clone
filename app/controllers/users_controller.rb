b4ecde1c747f59bc49bfed55cc83f7ce2b3b0388
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 18:41:39 -0500

    add user edit profile functionality
    add user#edit #create actions in users controller
    add edit form for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to @user
   end
 
   def destroy
