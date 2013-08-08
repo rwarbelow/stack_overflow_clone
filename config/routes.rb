27cd3c5bea6ba616ae6ee6782635bdec1f786820
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 22:52:33 -0500

    add custom route for users_questions

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ StackOverflowClone::Application.routes.draw do
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get '/users/:user_id/questions', to: 'user_questions#index', as: 'user_questions'
 end
