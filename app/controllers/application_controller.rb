0e8fff8699c7e8f721df0b90ae93c35832a6d882
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 22:18:06 -0500

    include helper method for user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def current_user
-    @_current_user ||= User.find(session[:id]) if session[:id]
-  end
+  include ApplicationHelper
 end
