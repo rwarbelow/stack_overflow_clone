23a3c9b2721eb72997b4dbdf96665afa40969596
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 21:44:04 -0500

    add associations for user questions and answers
    user has many questions and answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   validates :name,  presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
-  
+
+  has_many :questions
+  has_many :answers
+
   has_secure_password
 end
