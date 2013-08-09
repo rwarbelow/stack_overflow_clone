cf6a6dcaa0fe112455c16d8c8ce53c3fa72bc66e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-09 11:31:03 -0500

    add voting to answers
    add forms for each answer (vote up, cancel vote, vote down)
    refactor votes_controller to use params[:commentable_type],
    so that controller code is reusable and doesn't hard-code in
    the commentable_type and vote#create is usable for questions and
    answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,10 @@
 class VotesController < ApplicationController
   def create
-    user_questions_votes = current_user.votes.where(voteable_type:params[:vote][:voteable_type])
-    this_question_user_vote = user_questions_votes.find_or_create_by_voteable_id(params[:vote][:voteable_id])
-    this_question_user_vote.update_attributes(direction: params[:vote][:direction])
+    if current_user
+      user_questions_votes = current_user.votes.where(voteable_type:params[:vote][:voteable_type])
+      this_question_user_vote = user_questions_votes.find_or_create_by_voteable_id(params[:vote][:voteable_id])
+      this_question_user_vote.update_attributes(direction: params[:vote][:direction])
+    end
     redirect_to :back
   end
 end
