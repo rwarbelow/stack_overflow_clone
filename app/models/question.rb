77d386d9d3c55066b9dc89271ab5a8ae639e2019
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-09 17:37:58 -0500

    added validation of presence for best answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   attr_accessible :text, :user_id, :title, :all_tags
   
   belongs_to :user
+  belongs_to :best_answer, class_name: "Answer"
   has_many :answers
   has_many :question_tags
   has_many :tags, through: :question_tags
