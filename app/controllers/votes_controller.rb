9198295597b91780547c982cfbf03a6f1ed11ece
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-09 11:48:33 -0500

    move Vote#create logic into Vote model method
    refactor to move controller logic into the model
    pass in params and the current_user.id

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,7 @@
 class VotesController < ApplicationController
   def create
     if current_user
-      user_questions_votes = current_user.votes.where(voteable_type:params[:vote][:voteable_type])
-      this_question_user_vote = user_questions_votes.find_or_create_by_voteable_id(params[:vote][:voteable_id])
-      this_question_user_vote.update_attributes(direction: params[:vote][:direction])
+      Vote.allow_vote( params[:vote], current_user.id )
     end
     redirect_to :back
   end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,11 @@ class Vote < ActiveRecord::Base
   attr_accessible :voteable_type, :voteable_id, :direction
   belongs_to :voteable, polymorphic: true
   belongs_to :user
+
+  def self.allow_vote(params, user_id)
+    current_user = User.find(user_id)
+    user_questions_votes = current_user.votes.where(voteable_type: params[:voteable_type])
+    this_question_user_vote = user_questions_votes.find_or_create_by_voteable_id(params[:voteable_id])
+    this_question_user_vote.update_attributes(direction: params[:direction])
+  end
 end
