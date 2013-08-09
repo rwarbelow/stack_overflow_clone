384f60c0a2b5a7f9a1c9ecf7500bb402edff1c19
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 20:56:09 -0500

    setup polymorphic associations with vote, answer, comment, question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
   has_many :comments, as: :commentable
+  has_many :votes,    as: :voteable
 
   validates_presence_of :text
 end
