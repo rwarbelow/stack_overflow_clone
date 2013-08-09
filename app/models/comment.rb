b0b9c905c8f386e60a83d36d605cfd3b8c3e9df3
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 21:09:21 -0500

    add comment model and migration (for real)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :text
   belongs_to :commentable, polymorphic: true
+  belongs_to :user
 end
--- db/migrate/20130809013856_create_comments.rb
+++ db/migrate/20130809013856_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :text
       t.references :commentable, polymorphic: true
+      t.references :user
       t.timestamps
     end
   end
