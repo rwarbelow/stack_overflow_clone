e04fb4eefde227f18903bbdfd2de7e45e1e32d19
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-09 16:34:53 -0500

    added validations for password_confirmation presence, and email regex and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   validates :name,  presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, format: {with: /.+@.+\..+/}, uniqueness: true
 
   has_many :questions
   has_many :answers
