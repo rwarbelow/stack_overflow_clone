331bbf80b86270e559166535faf8c006b5d07bc0
kevinbuch <kevaustinbuch@gmail.com>
2013-08-09 16:12:37 -0500

    added logic to return partial for ajax vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,8 @@
 class VotesController < ApplicationController
   def create
     if current_user
-      Vote.allow_vote( params[:vote], current_user.id )
+      @vote = Vote.allow_vote( params[:vote], current_user.id )
     end
-    redirect_to :back
+    render partial: 'show', layout: false, locals: {voteable: @vote.voteable, vote: Vote.new }
   end
 end
